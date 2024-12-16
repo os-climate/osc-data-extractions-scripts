b69d1592f97b92b893d828672120c79e0808ab4f
Matthew Watkins <mwatkins@linuxfoundation.org>
2024-12-16 21:53:54 +0000

    Chore: Remove docker-compose.yml
    Signed-off-by: Matthew Watkins <mwatkins@linuxfoundation.org>

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
 
 # SPDX-License-Identifier: Apache-2.0
 # Copyright 2024 The Linux Foundation
@@ -13,31 +13,34 @@ set -o pipefail
 ### Variables
 
 SOURCE="inputs"
-DESTINATION="outputs"
 SELECTION="e15*.pdf"
 
 ### Functions
 
 _process_files() {
-  echo "Processing: $1"
-  sleep 3
+    echo "Processing: $1"
+    sleep 3
 }
 export -f _process_files
 
 NPROC_CMD=$(which nproc)
 if [ -x "$NPROC_CMD" ]; then
-	THREADS=$($NPROC_CMD)
+    THREADS=$($NPROC_CMD)
 else
-	echo "Error: nproc command not found in PATH"
-	exit 1
+    echo "Error: nproc command not found in PATH"
+    exit 1
 fi
 
 echo "OS-Climate / Data Extraction Team"
 echo "Bulk execution script"
 echo "Parallel threads for batch processing: $THREADS"
 START=$(date '+%s')
-echo -n "Input files to process: "; ls $SOURCE/$SELECTION | wc -l
-ls $SOURCE/$SELECTION | parallel -j "$THREADS" _process_files
+echo -n "Input files to process: "
+# shellcheck disable=SC2012
+FILES=$(ls "$SOURCE/$SELECTION")
+echo "$FILES" | wc -l
+# shellcheck disable=SC2012
+echo "$FILES" | parallel -j "$THREADS" _process_files
 END=$(date '+%s')
 ELAPSED=$((END-START))
 echo "Elapsed time in seconds: $ELAPSED"
